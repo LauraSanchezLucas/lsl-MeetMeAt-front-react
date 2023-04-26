76cc4e27bd5209fc10e003abe7b7b75f693eefa9
Laura <Lausnclu@hotmail.com>
2023-04-26 11:17:56 +0200

    chore: clear and organize code to main and apiCalls

--- MeetMeAt/src/MainApp.jsx
+++ MeetMeAt/src/MainApp.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Home } from './layout/home/Home'
-import { Register } from './layout/register/Register'
-import { Login } from './layout/login/Login'
-import { NavBarComponent } from './components/navbar/NavbarComponent'
-import { ProfileUser } from './layout/profile/ProfileUser'
-import { SeeAllAppointment } from './layout/user/SeeAllAppointments'
-import { BookAppointment } from './layout/user/BookAppointment'
-import { CreateEventByAdmin } from './layout/eventLayout/CreateEventByAdmin'
-import { CreateRole } from './layout/role/CreateRole'
-import { SeeAllRoles } from './layout/role/SeeAllRoles'
-import { Business } from './layout/business/business'
-import { CreateBusiness } from './layout/business/CreateBusiness'
-import { SeeAllUserByAdmin } from './layout/user/SeeAllUserByAdmin'
-import { SeeAllAppointmentByAdmin } from './layout/appointment/SeeAllAppointmentByAdmin'
-import { CreateAppointmentByAdmin } from './layout/appointment/CreateAppointmentByAdmin'
-import { CreateEventByProfessional } from './layout/eventLayout/CreateEventByProfessional'
-import { SeeAllAppointmentByProfessional } from './layout/appointment/SeeAllAppointmentByProfessional'
-import { CreateUserByAdmin } from './layout/user/CreateUserByAdmin'
-import { UpdateProfile } from './layout/user/UpdateProfile'
-import { SeeEvents } from './layout/eventLayout/SeeEvents'
-import { SeeAllEventsByProfessional } from './layout/eventLayout/SeeAllEventsByProfessional'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './layout/home/Home';
+import { Register } from './layout/register/Register';
+import { Login } from './layout/login/Login';
+import { NavBarComponent } from './components/navbar/NavbarComponent';
+import { ProfileUser } from './layout/profile/ProfileUser';
+import { SeeAllAppointment } from './layout/user/SeeAllAppointments';
+import { BookAppointment } from './layout/user/BookAppointment';
+import { CreateEventByAdmin } from './layout/eventLayout/CreateEventByAdmin';
+import { CreateRole } from './layout/role/CreateRole';
+import { SeeAllRoles } from './layout/role/SeeAllRoles';
+import { Business } from './layout/business/business';
+import { CreateBusiness } from './layout/business/CreateBusiness';
+import { SeeAllUserByAdmin } from './layout/user/SeeAllUserByAdmin';
+import { SeeAllAppointmentByAdmin } from './layout/appointment/SeeAllAppointmentByAdmin';
+import { CreateAppointmentByAdmin } from './layout/appointment/CreateAppointmentByAdmin';
+import { CreateEventByProfessional } from './layout/eventLayout/CreateEventByProfessional';
+import { SeeAllAppointmentByProfessional } from './layout/appointment/SeeAllAppointmentByProfessional';
+import { CreateUserByAdmin } from './layout/user/CreateUserByAdmin';
+import { UpdateProfile } from './layout/user/UpdateProfile';
+import { SeeEvents } from './layout/eventLayout/SeeEvents';
+import { SeeAllEventsByProfessional } from './layout/eventLayout/SeeAllEventsByProfessional';
 
 
 
@@ -28,30 +28,29 @@ import { SeeAllEventsByProfessional } from './layout/eventLayout/SeeAllEventsByP
 export const MainApp = () => {
   return (
     <div>
-      <NavBarComponent/>
-    <Routes>
-      <Route path='/' element={ <Home/> }/>
-      <Route path= '/register' element={ <Register/> }/>
-      <Route path= '/login' element={ <Login/> }/>
-      <Route path= '/profile' element={ <ProfileUser/> }/>
-      {/* eliminar la ruta de todos los eventos porque ya sale al reservar y cambiarle el nombre en el navbar de new appointment a todos los eventos */}
-      <Route path= '/all/events' element={ <SeeEvents/> }/>
-      <Route path= '/create/appointment' element={ <BookAppointment/> }/>
-      <Route path= '/appointment' element={ <SeeAllAppointment/> }/>
-      <Route path= '/newevent' element={ <CreateEventByAdmin/> }/>
-      <Route path= '/newrole' element={ <CreateRole/> }/>
-      <Route path= '/role' element={ <SeeAllRoles/> }/>
-      <Route path= '/business' element={ <Business/> }/>
-      <Route path= '/newbusiness' element={ <CreateBusiness/> }/>
-      <Route path= '/allusers' element={ <SeeAllUserByAdmin/> }/>
-      <Route path= '/getappointment' element={ <SeeAllAppointmentByAdmin/> }/>
-      <Route path= '/newappointment' element={ <CreateAppointmentByAdmin/> }/>
-      <Route path= '/neweventprofessional' element={ <CreateEventByProfessional/> }/>
-      <Route path= '/getappointmentbyprofess' element={ <SeeAllAppointmentByProfessional/> }/>
-      <Route path= '/newuseradmin' element={ <CreateUserByAdmin/> }/>
-      <Route path= '/update/profile' element={ <UpdateProfile/> }/>
-      <Route path= '/all/events/professional' element={ <SeeAllEventsByProfessional/> }/>
-    </Routes>
+      <NavBarComponent />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<ProfileUser />} />
+        <Route path='/all/events' element={<SeeEvents />} />
+        <Route path='/create/appointment' element={<BookAppointment />} />
+        <Route path='/appointment' element={<SeeAllAppointment />} />
+        <Route path='/newevent' element={<CreateEventByAdmin />} />
+        <Route path='/newrole' element={<CreateRole />} />
+        <Route path='/role' element={<SeeAllRoles />} />
+        <Route path='/business' element={<Business />} />
+        <Route path='/newbusiness' element={<CreateBusiness />} />
+        <Route path='/allusers' element={<SeeAllUserByAdmin />} />
+        <Route path='/getappointment' element={<SeeAllAppointmentByAdmin />} />
+        <Route path='/newappointment' element={<CreateAppointmentByAdmin />} />
+        <Route path='/neweventprofessional' element={<CreateEventByProfessional />} />
+        <Route path='/getappointmentbyprofess' element={<SeeAllAppointmentByProfessional />} />
+        <Route path='/newuseradmin' element={<CreateUserByAdmin />} />
+        <Route path='/update/profile' element={<UpdateProfile />} />
+        <Route path='/all/events/professional' element={<SeeAllEventsByProfessional />} />
+      </Routes>
     </div>
   )
 }
--- MeetMeAt/src/service/apiCalls.js
+++ MeetMeAt/src/service/apiCalls.js
@@ -4,98 +4,21 @@ const root = "http://localhost:3000"
 
 // REGISTER
 export const registerMe = async (body) => {
-    return await axios.post(`${root}/register`, body);
+  return await axios.post(`${root}/register`, body);
 };
 // LOGIN
 export const logMe = async (body) => {
-    return await axios.post(`${root}/login`, body);
+  return await axios.post(`${root}/login`, body);
 };
-
+// ----------------------------------USERS-----------------------------------------------------
 // SEE PROFILE BY USER
 export const getUserProfile = async (token) => {
-    let config = {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    };
-    return await axios.get(`${root}/profile`, config);
-};
-// GET ALL EVENTS ALL
-export const getAllEvents = async () => {
-
-    return await axios.get(`${root}/all/events`);
-};
-// CREATE APPOINTMENT BY USER
-export const getAppointment = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    return await axios.post(`${root}/create/appointment`, body, config);
-};
-
-// SEE ALL APPOINTMENTS BY USER
-export const seeAppointment = async (token) => {
-    let config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    return await axios.get(`${root}/appointment`, config);
-  };
-
-  // CREATE EVENT BY ADMIN
-export const createEvents = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    return await axios.post(`${root}/newevent`, body, config);
-};
-  // CREATE EVENT BY PROFESIONAL
-  export const createEventsByProfessional = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    return await axios.post(`${root}/neweventprofessional`, body, config);
-};
-  // CREATE ROLE BY ADMIN
-  export const createRole = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    return await axios.post(`${root}/newrole`, body, config);
-};
-
-// SEE ALL ROLES BY ADMIN
-export const getAllRoles = async (token) => {
   let config = {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: "Bearer " + token,
     },
   };
-  return await axios.get(`${root}/role`, config);
-};
-
-// SEE ALL BUSINESS BY ADMIN
-export const getAllBusinesses = async () => {
-
-  return await axios.get(`${root}/business`);
-};
-  // CREATE BUSINESS BY ADMIN
-  export const createBusinesses = async (body, token) => {
-    let config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    return await axios.post(`${root}/newbusiness`, body, config);
+  return await axios.get(`${root}/profile`, config);
 };
 // SEE ALL USERS BY ADMIN
 export const getAllUsers = async (token) => {
@@ -106,35 +29,6 @@ export const getAllUsers = async (token) => {
   };
   return await axios.get(`${root}/allusers`, config);
 };
-// SEE ALL APPOINTMENTS BY ADMIN
-export const getAllAppointmentsAdmin = async (token) => {
-  let config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  return await axios.get(`${root}/getappointment`, config);
-};
-  // CREATE APPOINTMENT BY ADMIN
-  export const createAppointmentAdmin = async (body, token) => {
-      let config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      return await axios.post(`${root}/newappointment`, body, config);
-  };
-
-
-  // SEE ALL APPOINTMENTS BY PROFESSIONAL
-export const getAllAppointmentsProfessional = async (token) => {
-  let config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  return await axios.get(`${root}/getappointmentbyprofess`, config);
-};
 // REGISTER USER BY ADMIN
 export const registerByAdmin = async (body, token) => {
   let config = {
@@ -144,28 +38,56 @@ export const registerByAdmin = async (body, token) => {
   };
   return await axios.post(`${root}/newuseradmin`, body, config);
 };
-
-// DELETE APPOINTMENT BY USER
-
-export const deleteAppointmentUser = async(id, token) =>{
-  const config = {
-    headers:{
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  return await axios.delete(`${root}/cancelappointment/${id}`, config);
-};
-
 // UPDATE PROFILE USER
-export const updateProfileUser = async(body,token) =>{
+export const updateProfileUser = async (body, token) => {
   const config = {
-    headers:{
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
   return await axios.put(`${root}/update/profile`, body, config);
 };
-
+// DELETE USER BY ADMIN
+export const deleteUserByAdmin = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.delete(`${root}/cancelluser/${id}`, config);
+};
+// UPDATE USER BY ADMIN
+export const updateUserByAdmin = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.put(`${root}/updateuserprofile/${id}`, config);
+};
+// ------------------------------------EVENTS--------------------------------------------------
+// GET ALL EVENTS ALL
+export const getAllEvents = async () => {
+  return await axios.get(`${root}/all/events`);
+};
+// CREATE EVENTS BY ADMIN
+export const createEvents = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.post(`${root}/newevent`, body, config);
+};
+// CREATE EVENTS BY PROFESIONAL
+export const createEventsByProfessional = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.post(`${root}/neweventprofessional`, body, config);
+};
 // SEE MY OWN EVENTS BY PROFESSIONAL
 export const getAllEventsProfessional = async (token) => {
   let config = {
@@ -175,65 +97,136 @@ export const getAllEventsProfessional = async (token) => {
   };
   return await axios.get(`${root}/all/events/professional`, config);
 };
-
-// DELETE EVENT BY PROFESSIONAL
-
-export const deleteEventByProfessional = async(id, token) =>{
+// DELETE EVENTS BY PROFESSIONAL
+export const deleteEventByProfessional = async (id, token) => {
   const config = {
-    headers:{
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
   return await axios.delete(`${root}/deleteeventprofessi/${id}`, config);
 };
-// DELETE EVENT BY ADMIN
-
-export const deleteEventByAdmin= async(id, token) =>{
+// DELETE EVENTS BY ADMIN
+export const deleteEventByAdmin = async (id, token) => {
   const config = {
-    headers:{
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
   return await axios.delete(`${root}/deleteevent/${id}`, config);
 };
-
-// DELETE BUSINESS BY ADMIN
-
-export const deleteBusinessById= async(id, token) =>{
-  const config = {
-    headers:{
+// -----------------------------------APPOINTMENTS--------------------------------------------
+// CREATE APPOINTMENTS BY USER
+export const getAppointment = async (body, token) => {
+  let config = {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
-  return await axios.delete(`${root}/deletebusiness/${id}`, config);
+  return await axios.post(`${root}/create/appointment`, body, config);
 };
-// DELETE USER BY ADMIN
-
-export const deleteUserByAdmin = async(id, token) =>{
-  const config = {
-    headers:{
+// SEE ALL APPOINTMENTS BY ADMIN
+export const getAllAppointmentsAdmin = async (token) => {
+  let config = {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
-  return await axios.delete(`${root}/cancelluser/${id}`, config);
+  return await axios.get(`${root}/getappointment`, config);
 };
-// DELETE APPOINTMENT BY ADMIN
-
-export const deleteAppointmentById = async(id, token) =>{
+// CREATE APPOINTMENTS BY ADMIN
+export const createAppointmentAdmin = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.post(`${root}/newappointment`, body, config);
+};
+// SEE ALL APPOINTMENTS BY PROFESSIONAL
+export const getAllAppointmentsProfessional = async (token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.get(`${root}/getappointmentbyprofess`, config);
+};
+// SEE ALL APPOINTMENTS BY USER
+export const seeAppointment = async (token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.get(`${root}/appointment`, config);
+};
+// DELETE APPOINTMENTS BY USER
+export const deleteAppointmentUser = async (id, token) => {
   const config = {
-    headers:{
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.delete(`${root}/cancelappointment/${id}`, config);
+};
+// DELETE APPOINTMENTS BY ADMIN
+export const deleteAppointmentById = async (id, token) => {
+  const config = {
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
   return await axios.delete(`${root}/deleteappointment/${id}`, config);
 };
-
-// UPDATE ROLE ADMIN
-export const updateRoleByAdmin = async(id,token) =>{
+// ----------------------------------ROLES-----------------------------------------------------
+// CREATE ROLES BY ADMIN
+export const createRole = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.post(`${root}/newrole`, body, config);
+};
+// SEE ALL ROLES BY ADMIN
+export const getAllRoles = async (token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.get(`${root}/role`, config);
+};
+// UPDATE ROLES ADMIN
+export const updateRoleByAdmin = async (id, token) => {
   const config = {
-    headers:{
+    headers: {
       Authorization: `Bearer ${token}`,
     },
   };
   return await axios.put(`${root}/updateuserrole/${id}`, id, config);
+};
+// ---------------------------------BUSINESS-------------------------------------------------
+// SEE ALL BUSINESSES BY ADMIN
+export const getAllBusinesses = async () => {
+  return await axios.get(`${root}/business`);
+};
+// CREATE BUSINESSES BY ADMIN
+export const createBusinesses = async (body, token) => {
+  let config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.post(`${root}/newbusiness`, body, config);
+};
+// DELETE BUSINESSES BY ADMIN
+export const deleteBusinessById = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return await axios.delete(`${root}/deletebusiness/${id}`, config);
 };
