5f3d835d8b868ea84b29f24cd113bb2329e93d69
Laura <Lausnclu@hotmail.com>
2023-04-26 18:27:30 +0200

    Chore: Change setTimeout for reload window in see all user by admin

--- MeetMeAt/src/components/navbar/NavbarComponent.jsx
+++ MeetMeAt/src/components/navbar/NavbarComponent.jsx
@@ -2,95 +2,97 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, useNavigate } from 'react-router-dom';
-import Image from "react-bootstrap/Image";
 import { useDispatch, useSelector } from 'react-redux';
 import { userData } from '../../layout/userSlice';
 import { userout } from '../../layout/userSlice';
-import NavDropdown from "react-bootstrap/NavDropdown";
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
 
 
 export const NavBarComponent = () => {
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const dataCredentialRdx = useSelector(userData);
-    // console.log(dataCredentialRdx)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const dataCredentialRdx = useSelector(userData);
+ 
 
-    const logout = () => {
-        // dispatch(logout(dataCredentialsRdx = ""));
-        dispatch(userout({ credentials: {}, token: '' }));
-        return navigate("/");
-    };
+  const logout = () => {
+    dispatch(userout({ credentials: {}, token: '' }));
+    return navigate('/');
+  };
+ 
+  return (
+    <Navbar collapseOnSelect expand="sm">
+      <Container>
+        <Navbar.Toggle/>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link as={Link} to="/" >Home</Nav.Link>
+            {!dataCredentialRdx?.credentials?.role ? (
+              <>
+                <Nav.Link as={Link} to="/register" >Sign Up!</Nav.Link>
+                <Nav.Link as={Link} to="/login" ><i className="bi bi-person-circle"></i></Nav.Link>
+              </>
+            ) :
+              dataCredentialRdx?.credentials?.role === 3 ? (
+                <>
+                
+                <Nav.Link as={Link} to="/create/appointment" >Events</Nav.Link>
+                <Nav.Link as={Link} to="/appointment" >MY EVENTS</Nav.Link>
+                  <NavDropdown title="Menu" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/create/appointment">Events</NavDropdown.Item>
+                    <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="/update/profile">Update Profile</NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link>{dataCredentialRdx.credentials.user.name}</Nav.Link>
+                  <Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>
+                </>
+              ) :
+                dataCredentialRdx?.credentials?.role === 2 ? (
+                  <>
+                    <NavDropdown title="Menu" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="/neweventprofessional">Create Event</NavDropdown.Item>
+                      <NavDropdown.Item href="/all/events/professional" >All events</NavDropdown.Item>
+                      <NavDropdown.Item href="/getappointmentbyprofess" >Get all user into my events</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>
+                  </>
+                ) :
+                  dataCredentialRdx?.credentials?.role === 1 ? (
+                    <>
+                      <NavDropdown title="Event" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/all/events" >All events</NavDropdown.Item>
+                        <NavDropdown.Item href="/newevent">Create event</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Update event</NavDropdown.Item>
+                      </NavDropdown>
+                      <NavDropdown title="Roles" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/role">All roles</NavDropdown.Item>
+                        <NavDropdown.Item href="/newrole">Create role</NavDropdown.Item>
+                      </NavDropdown>
+                      <NavDropdown title="Business" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/business">All business</NavDropdown.Item>
+                        <NavDropdown.Item href="/newbusiness">Create Business</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Update business</NavDropdown.Item>
+                      </NavDropdown>
+                      <NavDropdown title="User" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/allusers">All user no profesional</NavDropdown.Item>
+                        <NavDropdown.Item href="/newuseradmin">Create user</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Update user</NavDropdown.Item>
 
-    return (
-        <Navbar collapseOnSelect expand="sm">
-            <Container>
-                <Nav.Link as={Link} to="/">MeetMeAt</Nav.Link>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav>
-                        <Nav.Link as={Link} to="/" >Home</Nav.Link>
-                        {!dataCredentialRdx?.credentials?.role ? (
-                            <>
-                                <Nav.Link as={Link} to="/login" >Login</Nav.Link>
-                                <Nav.Link as={Link} to="/register" >Register</Nav.Link>
-                            </>
-                        ) :
-                            dataCredentialRdx?.credentials?.role === 3 ? (
-                                <>
-                                    <NavDropdown title="Menu" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                                        <NavDropdown.Item href="/create/appointment">New Appointment</NavDropdown.Item>
-                                        <NavDropdown.Item href="/appointment">See all appointment to event</NavDropdown.Item>
-                                        <NavDropdown.Item href="/update/profile">Update Profile</NavDropdown.Item>
-                                    </NavDropdown>
-                                    <Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>
-                                </>
-                            ) :
-                                dataCredentialRdx?.credentials?.role === 2 ? (
-                                    <>
-                                    <NavDropdown title="Menu" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="/neweventprofessional">Create Event</NavDropdown.Item>
-                                        <NavDropdown.Item href="/all/events/professional" >All events</NavDropdown.Item>
-                                        <NavDropdown.Item href="/getappointmentbyprofess" >Get all user into my events</NavDropdown.Item>
-                                    </NavDropdown>
-                                        <Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>
-                                    </>
-                                ) :
-                                    dataCredentialRdx?.credentials?.role === 1 ? (
-                                        <>
-                                        <NavDropdown title="Event" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/all/events" >All events</NavDropdown.Item>
-                                            <NavDropdown.Item href="/newevent">Create event</NavDropdown.Item>
-                                            <NavDropdown.Item href="/">Update event</NavDropdown.Item>
-                                        </NavDropdown>
-                                        <NavDropdown title="Roles" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/role">All roles</NavDropdown.Item>
-                                            <NavDropdown.Item href="/newrole">Create role</NavDropdown.Item>
-                                        </NavDropdown>
-                                        <NavDropdown title="Business" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/business">All business</NavDropdown.Item>
-                                            <NavDropdown.Item href="/newbusiness">Create Business</NavDropdown.Item>
-                                            <NavDropdown.Item href="/">Update business</NavDropdown.Item>
-                                        </NavDropdown>
-                                        <NavDropdown title="User" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/allusers">All user no profesional</NavDropdown.Item>
-                                            <NavDropdown.Item href="/newuseradmin">Create user</NavDropdown.Item>
-                                            <NavDropdown.Item href="/">Update user</NavDropdown.Item>
-
-                                        </NavDropdown>
-                                        <NavDropdown title="Appointment" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="/getappointment">All appointments</NavDropdown.Item>
-                                            <NavDropdown.Item href="/newappointment">Create appointment</NavDropdown.Item>
-                                            <NavDropdown.Item href="/">Update appontment</NavDropdown.Item>
-                                        </NavDropdown>
-                                            <Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>
-                                        </>
-                                    ) : (<Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>)
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+                      </NavDropdown>
+                      <NavDropdown title="Appointment" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/getappointment">All appointments</NavDropdown.Item>
+                        <NavDropdown.Item href="/newappointment">Create appointment</NavDropdown.Item>
+                        <NavDropdown.Item href="/">Update appontment</NavDropdown.Item>
+                      </NavDropdown>
+                      <Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>
+                    </>
+                  ) : (<Nav.Link as={Link} to="/" onClick={() => logout()}>Logout</Nav.Link>)
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
+
--- MeetMeAt/src/layout/user/SeeAllUserByAdmin.jsx
+++ MeetMeAt/src/layout/user/SeeAllUserByAdmin.jsx
@@ -7,9 +7,9 @@ import { Button } from "react-bootstrap";
 import { UpdateUserAdminM } from "../../components/modal/UpdateUserAdminM";
 
 export const SeeAllUserByAdmin = () => {
-    
+
     const navigate = useNavigate();
-    
+
     const credentialRdx = useSelector(userData);
 
     const [users, setUsers] = useState([]);
@@ -21,63 +21,60 @@ export const SeeAllUserByAdmin = () => {
     const dispatch = useDispatch();
 
 
-    useEffect(()=>{
-        if(users.length === 0){
+    useEffect(() => {
+        if (users.length === 0) {
             getAllUsers(credentialRdx.credentials.token)
-            .then(
-                result => {
-                    console.log(result.data.user, 'tttttttt')
-                    setUsers(result.data.user)
-                }
-            )
-            .catch(error => console.log(error));
+                .then(
+                    result => {
+                        console.log(result.data.user, 'tttttttt')
+                        setUsers(result.data.user)
+                    }
+                )
+                .catch(error => console.log(error));
         }
         console.log(users)
     }, [users])
 
     const selected = (user) => {
-        //Primero guardo en RDX los datos escogidos...
-        // dispatch(addChoosen({ choosenObject: appointment }))
         deleteUserByAdmin(user.id, credentialRdx.credentials.token)
-        console.log(user.id,'laura')
-        setTimeout(()=>{
-            navigate("/");
-        },500)
+        console.log(user.id, 'laura')
+        window.location.reload(true);
     }
-  return (
-    <>
-    <div>
-    {  users.length > 0 ? 
 
-        (<div>
-            {
-                users.map(
-                    user => {
-                        return (
-                            <div key={user.id}>
-                                    <ul>
-                            <strong>Name:</strong> &nbsp; {user.name} &nbsp;
-                            <strong>Surname:</strong> &nbsp; {user.surname} &nbsp;
-                            <strong>Email:</strong> &nbsp; {user.email} &nbsp;
-                            <strong>Phone:</strong> &nbsp; {user.phone} &nbsp;
-                            <strong>Role:</strong> &nbsp; {user.role_id} &nbsp;
-                            <Button onClick={()=>selected(user)}>Cancel!</Button>
-                            <button className='btn btn-outline-success'type='submit' onClick={handleShow}>Update event</button>
-                            </ul>
-                            </div>
-                        )
-                    }
-                )
-            }
-        </div>)
+    return (
+        <>
+            <div>
+                {users.length > 0 ?
 
-        : 
+                    (<div>
+                        {
+                            users.map(
+                                user => {
+                                    return (
+                                        <div key={user.id}>
+                                            <ul>
+                                                <strong>Name:</strong> &nbsp; {user.name} &nbsp;
+                                                <strong>Surname:</strong> &nbsp; {user.surname} &nbsp;
+                                                <strong>Email:</strong> &nbsp; {user.email} &nbsp;
+                                                <strong>Phone:</strong> &nbsp; {user.phone} &nbsp;
+                                                <strong>Role:</strong> &nbsp; {user.role_id} &nbsp;
+                                                <Button onClick={() => selected(user)}>Cancel!</Button>
+                                                <button className='btn btn-outline-success' type='submit' onClick={handleShow}>Update event</button>
+                                            </ul>
+                                        </div>
+                                    )
+                                }
+                            )
+                        }
+                    </div>)
 
-        (<div>ESTAN VINIENDO</div>)
+                    :
 
-    }
-    <UpdateUserAdminM show={show} handleClose={handleClose}/>
- </div>
-  </>
-)
+                    (<div>ESTAN VINIENDO</div>)
+
+                }
+                <UpdateUserAdminM show={show} handleClose={handleClose} />
+            </div>
+        </>
+    )
 }
