255ce292cb4192e73818981563f0e94601c1bdc2
Laura <Lausnclu@hotmail.com>
2023-05-01 21:23:14 +0200

    Style: Creation style see all events by professional without background picture

--- MeetMeAt/src/layout/eventLayout/byProfessional/seeAllEvents/SeeAllEventsByProfessional.jsx
+++ MeetMeAt/src/layout/eventLayout/byProfessional/seeAllEvents/SeeAllEventsByProfessional.jsx
@@ -4,8 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { userData } from "../../../userSlice";
 import { addChoosen } from '../../../detailSlice';
 import { deleteEventByProfessional, getAllEventsProfessional } from "../../../../service/apiCalls";
-import { Button } from "react-bootstrap";
-import './SeeAllEventsByProfessional.css'
+import { Button, Card, Col, Row } from "react-bootstrap";
+// import './SeeAllEventsByProfessional.css'
 
 
 export const SeeAllEventsByProfessional = () => {
@@ -17,19 +17,19 @@ export const SeeAllEventsByProfessional = () => {
 
     const [events, setEvents] = useState([]);
 
-    useEffect(()=>{
-        if(events.length === 0){
+    useEffect(() => {
+        if (events.length === 0) {
             getAllEventsProfessional(credentialRdx.credentials.token)
-            .then(
-                result => {
-                    console.log(result, 'tttttttt')
-                    setEvents(result.data.event)
-                    console.log(result.data.event, 'rrrrrrrr')
-                }
-            )
-            .catch(error => console.log(error));
+                .then(
+                    result => {
+                        console.log(result, 'tttttttt')
+                        setEvents(result.data.event)
+                        console.log(result.data.event, 'rrrrrrrr')
+                    }
+                )
+                .catch(error => console.log(error));
         }
-        
+
     }, [events])
 
 
@@ -38,41 +38,29 @@ export const SeeAllEventsByProfessional = () => {
         // dispatch(addChoosen({ choosenObject: appointment }))
         deleteEventByProfessional(event.id, credentialRdx.credentials.token)
         console.log(credentialRdx.credentials.token, 'yuuuuuuuu')
-        setTimeout(()=>{
+        setTimeout(() => {
             navigate("/");
-        },500)
+        }, 500)
     }
-  return (
-    <div className='event-see-background'>
-    {events.length > 0 ? 
-
-        (<div className="event-see-container">
-            {events.map(event => {
-                return (
-                    <div 
-                    key={event.id} className='event-see-box'>
-                        <ul>
-                            <div><strong>{event.name}</strong></div>
-                            <div>{event.description}</div>
-                            <div><strong>Date:&nbsp;&nbsp;</strong>{event.date}</div>
-                            <div><strong>Hour:&nbsp;&nbsp;</strong>{event.hour}</div>
-                            <div><strong>Place:&nbsp;&nbsp;</strong>{event.place}</div>
-                            <Button className="buttonOk" onClick={()=>selected(event)}>Cancel!</Button>
-                            </ul>
-                            </div>
-                        )
-                    }
-                )
-            }
-    </div>)
-
-        : 
-
-        (<div className='iscoming'><p className='loading-message'>The fun is on its way-stay tuned!!!</p></div>)
-
-    }
-
- </div>
-)
+    return (
+        <div className='event-user-background'>
+            <Row className='card-main'>
+                {events.map((event) => (
+                    <Col key={event.id} lg={4} sm={4}>
+                        <Card className='card-event'>
+                            <Card.Img className='event-image' variant='top' src={event.image} />
+                            <Card.Body>
+                                <Card.Title>{event.name}</Card.Title>
+                                <Card.Text><strong>Description:</strong><br />{event.description}</Card.Text>
+                                <Card.Text><strong>Place:</strong><br />{event.place} </Card.Text>
+                                <Card.Text><strong>Date:</strong><br /> {event.date} </Card.Text>
+                                <Card.Text><strong>Hour:</strong><br /> {event.hour} </Card.Text>
+                                <Button className="buttonOk" onClick={() => selected(event)}>Cancel!</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
 }
-
